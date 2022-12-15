dd5d8e58434a0a3d12134edaea77578debcac5ae
Brandon Holman <bholmdev@gmail.com>
2022-12-15 12:20:57 -0800

    Home page fixes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,17 +13,14 @@ const HomePage = () => {
             <div className="embedVideo">
                 <iframe width="774" height="472.2" src="https://www.youtube-nocookie.com/embed/wjORorezVeQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
+            <hr />
             <div>
                 <h3>Securing Your Future!!</h3>
-                <p>Our focus is aimed on distributing and promoting IT, Security, Telecom, and Services from the world’s most leading technology providers.
-                <br />
+                <p>Our focus is aimed on distributing and promoting IT, Security, Telecom, and Services from the world's most leading technology providers.</p>
                 <p>Our experts strive on providing the highest quality, cost effective solutions, with prompt deliveries that serve the demanding and challenging market needs.</p>
-                <br />
                 <p>We thrive on the mission of enabling the best business results through IDEAS, PEOPLE and TECHNOLOGY.</p>
-                <br />
                 <p>As a 360 Degrees Turn Key Solutions Provider, we offer a full spectrum of services that includes Consulting, Design, Implementation, Integration and a whole host of value added Services.</p>
-                <br />
-                <p>Committed to our full service value proposition model, we pride ourselves with our strong alliances from the world’s most renowned technology providers to offer the most effective and reliable solutions to our customers.</p></p>
+                <p>Committed to our full service value proposition model, we pride ourselves with our strong alliances from the world's most renowned technology providers to offer the most effective and reliable solutions to our customers.</p>
             </div>
         </Container>
     );
