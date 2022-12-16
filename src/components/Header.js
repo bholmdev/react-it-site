31c60236fa65c1224da0a3229960c4500d4950e6
Brandon Holman <bholmdev@gmail.com>
2022-12-16 12:02:29 -0800

    Navbar responsiveness change

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,13 +15,13 @@ const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <Navbar dark color='primary' sticky='top' expand='lg'className="navbar__custom">
+        <Navbar dark color='primary' sticky='top' expand='lg' className="navbar__custom">
             <NavbarBrand className="ms-5" href="home">
                 <h1 className="mt-1">BHolm IT</h1>
             </NavbarBrand>
         <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
             <Collapse isOpen={menuOpen} navbar>
-                <Nav className="ms-auto" navbar>
+                <Nav className="mx-auto" navbar>
                     <NavItem>
                         <NavLink className="nav-link" to="/">
                             Home
