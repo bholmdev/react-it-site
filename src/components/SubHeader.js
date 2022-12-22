58a3010a28bdf6913cab59cf41bfdfbd552b16e6
Brandon Holman <bholmdev@gmail.com>
2022-12-21 18:10:09 -0800

    fixed subheader

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -7,13 +7,8 @@ const SubHeader = ({ current, detail }) => {
             <Col>
                 <Breadcrumb>
                     <BreadcrumbItem>
-                        <Link to="/">Home</Link>
+                        <Link to="/react-it-site">Home</Link>
                     </BreadcrumbItem>
-                    {detail && (
-                        <BreadcrumbItem>
-                            <Link to="/directory">Directory</Link>
-                        </BreadcrumbItem>
-                    )}
                     <BreadcrumbItem active>{current}</BreadcrumbItem>
                 </Breadcrumb>
                 <h2>{current}</h2>
